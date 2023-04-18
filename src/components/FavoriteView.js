3551db28ada9471ff16f040e4f391058e60c08d8
Nick Guzman <nicolas.guzman@webdevstudios.com>
2023-04-18 09:50:44 -0400

    added abstraction layer and inputs

--- src/components/FavoriteView.js
+++ src/components/FavoriteView.js
@@ -1,21 +1,42 @@
 import "./FavoriteView.css";
-import React, {useState, useEffect} from "react";
+import React, { useState } from "react";
 
 import { addToFirebase } from "../helpers/firebaseHelpers";
 
-const addFavorite = async (name, description) => {
-  addToFirebase({objectToSave: {name, description}}, "MiColeccionDePrueba");
-}
-
 const FavoriteView = () => {
- 
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+
+  const addFavorite = async () => {
+    addToFirebase(
+      { objectToSave: { name, description } },
+      "MiTerceraColeccion"
+    );
+  };
+
+  const onNameChange = (e) => {
+    setName(e.target.value);
+    if (e.target.value.length > 10) {
+      alert("Name is too long");
+      setName(e.target.value.substring(0, 10));
+    }
+  };
+
+  const onDescriptionChange = (e) => {
+    setDescription(e.target.value);
+  };
 
   return (
     <div>
-        <h1>Favorite View</h1>
-        <button onClick={() => addFavorite("Favorite Test Unisabana",
-         "Description de lo que vamos a guardar" )}>Add Favorite
-         </button>
+      <h1>Favorite View</h1>
+      {/* generate 2 input fields   */}
+      <input type="text" placeholder="Name" onChange={(e) => onNameChange(e)} />
+      <input
+        type="text"
+        placeholder="Description"
+        onChange={(e) => onDescriptionChange(e)}
+      />
+      <button onClick={() => addFavorite()}>Add Favorite</button>
     </div>
   );
 };
