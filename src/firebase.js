1a756e5c17ac9432fd1637fa76dfc1bc95a48303
Nick Guzman <nicolas.guzman@webdevstudios.com>
2023-04-20 13:25:20 -0400

    added env config

--- src/firebase.js
+++ src/firebase.js
@@ -8,13 +8,14 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // TODO:
 const firebaseConfig = {
-  apiKey: "AIzaSyC98vLCE7YNCXTV2Pw3bCNmfJZ-1G2jjgE",
-  authDomain: "unisabana-class.firebaseapp.com",
-  projectId: "unisabana-class",
-  storageBucket: "unisabana-class.appspot.com",
-  messagingSenderId: "186512867242",
-  appId: "1:186512867242:web:2b2579ee0414adace8b5d3"
-};
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID 
+  }
 
 // Initialize Firebase
 
